f76f30ad866dc86bcb5185be2fa6c8df716d84e6
jungse12 <54869093+jungse12@users.noreply.github.com>
2020-07-11 17:48:53 -0400

    Update settings.py

--- Ford_Project/settings.py
+++ Ford_Project/settings.py
@@ -115,8 +115,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
