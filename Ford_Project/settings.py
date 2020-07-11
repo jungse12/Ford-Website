74136496e4db78f7615d624b9ab024fa64948937
jungse12 <54869093+jungse12@users.noreply.github.com>
2020-07-11 18:37:29 -0400

    Update settings.py

--- Ford_Project/settings.py
+++ Ford_Project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+jez(mz=3fk7pe@gmeryp^iv=!axjv99&&xq^#@!%fukesyu@5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ford-onboarding.herokuapp.com','127.0.0.1']
 
